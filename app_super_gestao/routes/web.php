5f9c980f30c45bffaa7ea219a0c89ef90f35529e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-06 21:10:21 -0300

    Rotas: Sobre-nós e contato.

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, seja bem-vindo ao curso!';
 });
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre-nós.';
+});
+
+Route::get('/contato', function () {
+    return 'Contato.';
+});
+
+/* verbo http
+
+get
+post
+put
+patch
+delete
+options
+
+*/
