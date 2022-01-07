f8d3ffcc245978c192aff5931f65d59886e378f6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-06 22:33:09 -0300

    Agrupando rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -9,11 +9,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
-
 Route::get('/contato/{nome}/{categoria_id}', 
     function(
         string $nome = 'Desconhecido', 
@@ -22,11 +19,13 @@ Route::get('/contato/{nome}/{categoria_id}',
         echo "Estamos aqui: $nome - $categoria_id";
     }
 )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-
 Route::get('/login', function() { return 'Login'; });
-Route::get('/clientes', function() { return 'Clientes'; });
-Route::get('/fornecedores', function() { return 'Fornecedores'; });
-Route::get('/produtos', function() { return 'Produtos'; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; });
+    Route::get('/fornecedores', function() { return 'Fornecedores'; });
+    Route::get('/produtos', function() { return 'Produtos'; });
+});
 
 /* verbo http
 
