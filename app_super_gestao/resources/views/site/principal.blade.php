7076d5d3cdd4e27715ce57c9c61db01f53aa3d49
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-06 22:19:32 -0300

    Criando o menu de navegação

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,15 @@
 <h3>Principal (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>   
+    </li>
+
+    <li>
+        <a href="/sobre-nos">Sobre-nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
--- app_super_gestao/resources/views/site/sobre-nos.blade.php
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,15 @@
 <h3>Sobre-nós (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>   
+    </li>
+
+    <li>
+        <a href="/sobre-nos">Sobre-nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
