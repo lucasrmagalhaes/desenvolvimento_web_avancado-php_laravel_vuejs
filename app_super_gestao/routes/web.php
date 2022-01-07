4408dc4aecf06aa24679ef06b70f63dc3fd405d5
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-06 22:02:57 -0300

    Tratando parâmetros de rotas com expressões regulares

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -14,16 +14,14 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+Route::get('/contato/{nome}/{categoria_id}', 
     function(
         string $nome = 'Desconhecido', 
-        string $categoria = 'Informação', 
-        string $assunto = 'Contato', 
-        string $mensagem = 'Mensagem não Informada'
+        int $categoria_id = 1
     ) {
-        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+        echo "Estamos aqui: $nome - $categoria_id";
     }
-);
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
 /* verbo http
 
