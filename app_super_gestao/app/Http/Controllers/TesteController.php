5f04a57d45e669fc61c2d6ebe2aaba173cc56beb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-13 20:54:38 -0300

    Encaminhando parâmetros do controlador para visualização

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2) {
-        echo "A soma de $p1 + $p2 é: " . ($p1 + $p2);
+        // echo "A soma de $p1 + $p2 é: " . ($p1 + $p2);
+
+        // return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // array associativo
+
+        // return view('site.teste', compact('p1', 'p2')); // compact - mais utilizado
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2); // with()
     }
 }
