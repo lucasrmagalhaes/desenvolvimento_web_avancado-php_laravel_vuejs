3a4eecc9d514af313b15175c8674cf726ddf2764
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-13 21:23:15 -0300

    Blade - @if/@else

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor.index');
+        $fornecedores = ['Fornecedor 1'];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -13,4 +13,25 @@
     */
 
     echo 'Texto de teste #3';
+    
+    /*
+    if () {
+
+    } elseif() {
+
+    } else {
+
+    }
+    */
 @endphp
+
+{{-- Sintaxe do blade não precisa ; --}}
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
