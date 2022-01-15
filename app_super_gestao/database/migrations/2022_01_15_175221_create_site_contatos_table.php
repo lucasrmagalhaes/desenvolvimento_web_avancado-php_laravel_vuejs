77334baac44519f1eba1d21000cf4a2b82503672
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-15 15:14:03 -0300

    Implementando a migration SiteContato

--- app_super_gestao/database/migrations/2022_01_15_175221_create_site_contatos_table.php
+++ app_super_gestao/database/migrations/2022_01_15_175221_create_site_contatos_table.php
@@ -15,7 +15,12 @@ class CreateSiteContatosTable extends Migration
     {
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps(); // Criação e atualização do registro
+            $table->string('nome', 50);
+            $table->string('telefone', 20);
+            $table->string('email', 80);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
         });
     }
 
