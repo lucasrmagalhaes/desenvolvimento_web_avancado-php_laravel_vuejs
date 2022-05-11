489754ccb5c732a66ec11ec5c18b150ed5faae35
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-10 21:44:46 -0300

    Eloquent - Ajustando o nome da tabela no Model para um correto ORM

--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Eloquent conversão:
+ * Site_Contato
+ * site_contato
+ * site_contatos
+ */
+
 class SiteContato extends Model
 {
-    //
+
 }
