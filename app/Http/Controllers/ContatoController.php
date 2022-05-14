7d3b0e294b97c0b069d207bb17fc2b6f6cb419be
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-13 23:19:36 -0300

    Validação de campos obrigatórios (required)

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,11 +7,19 @@ use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request) {
-        $contato = new SiteContato();
-
-        $contato->create($request->all());
-
+    public function contato() {
         return view('site.contato', ['titulo' => 'Contato']);
+    }
+
+    public function salvar(Request $request) {
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+
+        SiteContato::create($request->all());
     }
 }
