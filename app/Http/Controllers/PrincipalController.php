eec328edde55b512db26ac24c474b59d55ee701f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-17 13:17:54 -0300

    Ajustando o formulário de contato na rota principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        return view('site.principal');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.principal', [ 'motivo_contatos' => $motivo_contatos ]);
     }
 }
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -27,7 +27,13 @@
             <div class="contato">
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-                @component('site.layouts._components.form_contato', ['classe' => 'borda-branca'])
+                @component(
+                    'site.layouts._components.form_contato', 
+                    [
+                        'classe' => 'borda-branca',
+                        'motivo_contatos' => $motivo_contatos
+                    ]
+                )
                 @endcomponent
             </div>
         </div>
