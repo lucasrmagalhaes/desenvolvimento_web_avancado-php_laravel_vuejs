524658a49d8618777cc4a50c106d3a479cf8f4f3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-04 20:24:47 -0300

    Validação de campos e-mail

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -24,7 +24,7 @@ class ContatoController extends Controller
         $request->validate([
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
