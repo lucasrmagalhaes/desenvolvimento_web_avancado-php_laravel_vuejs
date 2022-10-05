fb648e678c159f796c7c396746eed428303f16d4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-04 21:58:39 -0300

    Criando o model LogAcesso e sua migration

---
+++ app/LogAcesso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogAcesso extends Model
+{
+    protected $fillable = ['log'];
+}
---
+++ database/migrations/2022_10_05_005025_create_log_acessos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogAcessosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_acessos', function (Blueprint $table) {
+            $table->id();
+            $table->string('log', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_acessos');
+    }
+}
