b0da220ce619d12daada30a444da8b66c7be793d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-08-22 18:33:32 -0300

    Manipulando a resposta requisição via middleware

--- super_gestao/app/Http/Middleware/LogAcessoMiddleware.php
+++ super_gestao/app/Http/Middleware/LogAcessoMiddleware.php
@@ -21,6 +21,11 @@ class LogAcessoMiddleware
 
         LogAcesso::create(['log' => "IP $ip requisitou a rota $rota."]);
 
-        return $next($request);
+        // return $next($request);
+
+        $resposta = $next($request);
+        $resposta->setStatusCode(201, 'O status da resposta e o texto da resposta foram modificados.');
+
+        return $resposta;
     }
 }
