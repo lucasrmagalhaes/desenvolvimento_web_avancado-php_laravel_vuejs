b3a022fc8d1c77f716ca8ebc670bc92b148748bb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-08-22 19:49:42 -0300

    Adicionando a função logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -59,6 +59,8 @@ class LoginController extends Controller
     }
 
     public function sair() {
-        echo 'Sair';
+        session_destroy();
+
+        return redirect()->route('site.index');
     }
 }
--- resources/views/app/layouts/_partials/topo.blade.php
+++ resources/views/app/layouts/_partials/topo.blade.php
@@ -9,6 +9,7 @@
             <li><a href="{{ route('app.cliente') }}">Cliente</a></li>
             <li><a href="{{ route('app.fornecedor') }}">Fornecedor</a></li>
             <li><a href="{{ route('app.produto') }}">Produto</a></li>
+            <li><a href="{{ route('app.sair') }}">Sair</a></li>
         </ul>
     </div>
 </div>
